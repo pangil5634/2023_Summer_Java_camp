7363e7e2e483d18f95009239d3f73d6a840ab7f1
pangil5634 <oksk6681@naver.com>
2023-07-12 00:58:58 +0900

    230712 C03 Complete

--- Main.java
+++ Main.java
@@ -3,28 +3,34 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Main stepC = new Main();
-        stepC.c02();
+        stepC.c03();
     }
 
-    void c02() {
-        double input_degree; // 입력받은 온도
-
+    void c03() {
+        int width, height; // 가로크기, 세로크기
         Scanner sc = new Scanner(System.in); // 스캐너 사용 선언
 
-        System.out.print("Enter water temperature > "); // 입력을 위한 출력 구문
-        input_degree = sc.nextDouble(); // input_degree 변수에 사용자로부터 값 입력 받기
+        System.out.print("Enter width of Rectangle > "); // 입력을 위한 출력 구문
+        width = sc.nextInt(); // width 변수에 사용자로부터 값 입력 받기
+        System.out.print("Enter height of Rectangle > "); // 입력을 위한 출력 구문
+        height = sc.nextInt(); // height 변수에 사용자로부터 값 입력 받기
 
-        if (input_degree < 0) { // 음수값인지
-            System.out.println("Invalid input!"); // 결과 출력 구문
-        } else if (input_degree < 25) { // 0도 이상 ~ 25도 미만인지
-            System.out.println("It's cold water"); // 결과 출력 구문
-        } else if (input_degree < 40) { // 25도 이상 ~ 40도 미만인지
-            System.out.println("It's warm water"); // 결과 출력 구문
-        } else if (input_degree < 80) { // 40도 이상 ~ 80도 미만인지
-            System.out.println("It's hot water"); // 결과 출력 구문
-        } else { // 80도 이상이면
-            System.out.println("It's boiling water"); // 결과 출력 구문
-        }
+        checkResult(width, height);// 직사각형 형태 판정 함수 호출
         sc.close(); // 스캐너 사용 해제
     }
+
+    void checkResult(int width, int height) { // 직사각형 형태 판정 함수
+        if (width == height)// 가로 크기와 세로 크기가 동일한지
+            System.out.println("It is square"); // 결과 출력 구문
+        else if (width >= height * 2) {// 가로 크기가 세로 크기의 2배 이상인지
+            System.out.println("It is an oblong rectangle from side to side."); // 결과 출력 구문
+        } else if (height >= width * 2) {// 세로 크기가 가로 크기의 2배 이상인지
+            System.out.println("It is an oblong rectangle from top to bottom."); // 결과 출력 구문
+        } else if (width > height) {// 가로 크기가 세로 크기보다 큰지
+            System.out.println("It is a horizontal rectangle."); // 결과 출력 구문
+        } else if (width < height) {// 세로 크기가 가로 크기보다 큰지
+            System.out.println("It is a vertical rectangle."); // 결과 출력 구문
+        }
+    }
+
 }
