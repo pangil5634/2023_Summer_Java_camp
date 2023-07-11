b1fc8f17a7953ba4825ebb0141c9b87c5100516f
pangil5634 <oksk6681@naver.com>
2023-07-12 03:18:03 +0900

    230712 C06 Complete

--- Main.java
+++ Main.java
@@ -3,69 +3,55 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Main stepC = new Main();
-        stepC.c05();
+        stepC.c06();
     }
 
-    void c05() {
-        int month, day; // 월, 일
+    void c06() {
+        int kor; // 국어점수
+        int eng; // 영어점수
+        int math; // 수학점수
+        int total; // 총점
+        double average; // 평균점수
 
-        int day_count = 0; // 1년 중 날 수
         Scanner sc = new Scanner(System.in); // 스캐너 사용 선언
 
-        System.out.print("Enter a month > "); // 입력을 위한 출력 구문
-        month = sc.nextInt(); // month 변수에 사용자로부터 값 입력 받기
-        System.out.print("Enter a day > "); // 입력을 위한 출력 구문
-        day = sc.nextInt(); // day 변수에 사용자로부터 값 입력 받기
+        System.out.print("Enter points of Korean > "); // 입력을 위한 출력 구문
+        kor = sc.nextInt(); // kor 변수에 사용자로부터 값 입력 받기
+        System.out.print("Enter points of English > ");// 입력을 위한 출력 구문
+        eng = sc.nextInt(); // eng 변수에 사용자로부터 값 입력 받기
+        System.out.print("Enter points of Math > ");// 입력을 위한 출력 구문
+        math = sc.nextInt(); // math 변수에 사용자로부터 값 입력 받기
 
-        if (month < 1 || month > 12 || day == 0) { // 입력한 달의 범위 및 day 범위 판단
-            System.out.println("This date is not valid."); // 범위를 벗어나면 출력하는 결과 출력 구문
-        } else { // 정상적으로 범위 안에 든 경우
+        total = kor + eng + math; // 합계 계산
 
-            boolean result = true; // 실행 여부 판단 변수
+        average = total / 3.0; // 평균 계산
 
-            // 각 월이 갖고 있는 일의 범위 판단 구문
-            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-                if (day > 31) {
-                    System.out.println("This date is not valid.");
-                    result = false;
-                }
-            } else if (month == 4 || month == 6 || month == 9 || month == 11) {
-                if (day > 30) {
-                    System.out.println("This date is not valid.");
-                    result = false;
-                }
-            } else if (month == 2) {
-                if (day > 28) {
-                    System.out.println("This date is not valid.");
-                    result = false;
-                }
-            }
+        System.out.println("Total point is " + total); // 결과 출력 구문
+        System.out.printf("The average is %.1f\n", average); // 결과 출력 구문
 
-            // 정상적으로 월과 일을 입력한 경우
-            if (result != false) {
-                for (int i = 1; i < month; i++) { // 입력한 달 이전까지의 달의 일 합산
-                    if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) {
-                        day_count += 31;
-                    } else if (i == 4 || i == 6 || i == 9 || i == 11) {
-                        day_count += 30;
-                    } else if (i == 2) {
-                        day_count += 28;
-                    }
-                }
-
-                day_count += day; // 입력한 일의 수까지 합산
-
-                // 결과 출력 구문
-                if (day_count == 1) {
-                    System.out.printf("This data is %dst of the year\n", day_count);
-                } else if (day_count == 2) {
-                    System.out.printf("This data is %dnd of the year\n", day_count);
-                } else if (day_count == 3) {
-                    System.out.printf("This data is %drd of the year\n", day_count);
-                } else
-                    System.out.printf("This date is %dth of the year\n", day_count);
-            }
-        }
-        sc.close(); // 스캐너 사용 해제
+        checkGrade(average); // 평균 점수의 등급 판단 함수 호출
+        sc.close(); // 스캐너 사용 종료
     }
+
+    void checkGrade(double average) { // 평균 점수의 등급 판단 함수
+        switch ((int) average / 10) { // 평균 점수를 10으로 나눈 값으로 등급 판단
+            case 9:
+                System.out.println("Your grade is A"); // 결과 출력 구문
+                break;
+            case 8:
+                System.out.println("Your grade is B"); // 결과 출력 구문
+                break;
+            case 7:
+                System.out.println("Your grade is C"); // 결과 출력 구문
+                break;
+            case 6:
+                System.out.println("Your grade is D"); // 결과 출력 구문
+                break;
+            default:
+                System.out.println("Your grade is F"); // 결과 출력 구문
+                break;
+        }
+
+    }
+
 }
