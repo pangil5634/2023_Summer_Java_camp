6fcb0f93c6d3937fb566c4e232f041a71b44a0e4
pangil5634 <oksk6681@naver.com>
2023-07-11 23:38:49 +0900

    230711 Upadte B09

--- StepB/B09.java
+++ StepB/B09.java
@@ -10,5 +10,24 @@ public class B09 {
 
     void b09() {
 
+        int height, weight; // 신장(cm), 체중(kg)
+        int bmi = 0; // 비만도 수치
+
+        Scanner sc = new Scanner(System.in); // 스캐너 사용 선언;
+        System.out.print("Enter height(cm) "); // 입력을 위한 출력 구문
+        height = sc.nextInt(); // height 변수에 사용자로부터 값 입력 받기
+        System.out.print("Enter weight(kg) "); // 입력을 위한 출력 구문
+        weight = sc.nextInt(); // weight 변수에 사용자로부터 값 입력 받기
+
+        bmi = weight / (height * height / 10000); // 값 계산
+
+        if (bmi >= 25) { // 비만인지
+            System.out.println("You are obesity"); // true
+        } else {
+            System.out.println("You are not obesity"); // false
+        }
+
+        sc.close(); // 스캐너 사용 해제;
     }
+
 }
