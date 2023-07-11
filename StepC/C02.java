60fd5fababd165e565874514ce663687a60ac2e8
pangil5634 <oksk6681@naver.com>
2023-07-12 00:34:27 +0900

    230712 C02 Complete

--- StepC/C02.java
+++ StepC/C02.java
@@ -9,6 +9,24 @@ public class C02 {
     }
 
     void c02() {
+        double input_degree; // 입력받은 온도
 
+        Scanner sc = new Scanner(System.in); // 스캐너 사용 선언
+
+        System.out.print("Enter water temperature > "); // 입력을 위한 출력 구문
+        input_degree = sc.nextDouble(); // input_degree 변수에 사용자로부터 값 입력 받기
+
+        if (input_degree < 0) { // 음수값인지
+            System.out.println("Invalid input!"); // 결과 출력 구문
+        } else if (input_degree < 25) { // 0도 이상 ~ 25도 미만인지
+            System.out.println("It's cold water"); // 결과 출력 구문
+        } else if (input_degree < 40) { // 25도 이상 ~ 40도 미만인지
+            System.out.println("It's warm water"); // 결과 출력 구문
+        } else if (input_degree < 80) { // 40도 이상 ~ 80도 미만인지
+            System.out.println("It's hot water"); // 결과 출력 구문
+        } else { // 80도 이상이면
+            System.out.println("It's boiling water"); // 결과 출력 구문
+        }
+        sc.close(); // 스캐너 사용 해제
     }
 }
