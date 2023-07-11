6e93107148819fc6c5ebbd6c7c873a688ac63396
pangil5634 <oksk6681@naver.com>
2023-07-12 02:55:33 +0900

    230712 Update C05

--- StepC/C05.java
+++ StepC/C05.java
@@ -9,6 +9,11 @@ public class C05 {
     }
 
     void c05() {
+        int month, day; // 월, 일
 
+        int day_count; // 1년 중 날 수
+        Scanner sc = new Scanner(System.in); // 스캐너 사용 선언
+
+        sc.close(); // 스캐너 사용 해제
     }
 }
