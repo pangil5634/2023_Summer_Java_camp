4da2bf3c27fa1fa5723b50adf077f3bb008b3f57
pangil5634 <oksk6681@naver.com>
2023-07-11 20:37:29 +0900

    230711 A06 Complete

--- StepA/A06.java
+++ StepA/A06.java
@@ -9,17 +9,28 @@ class A06 {
     }
 
     void a06() {
-        int days; // 날 수
-        int seconds; // 초 단위 시간
+        int kor; // 국어점수
+        int eng; // 영어점수
+        int math; // 수학점수
+        int total; // 총점
+        double average; // 평균점수
+
         Scanner sc = new Scanner(System.in); // 스캐너 사용 선언
 
-        System.out.print("Enter number of days > "); // 입력을 위한 출력 구문
-        days = sc.nextInt(); // days 변수에 사용자로부터 값 입력 받기
+        System.out.print("Enter points of Korean > "); // 입력을 위한 출력 구문
+        kor = sc.nextInt(); // kor 변수에 사용자로부터 값 입력 받기
+        System.out.print("Enter points of English > ");// 입력을 위한 출력 구문
+        eng = sc.nextInt(); // eng 변수에 사용자로부터 값 입력 받기
+        System.out.print("Enter points of Math > ");// 입력을 위한 출력 구문
+        math = sc.nextInt(); // math 변수에 사용자로부터 값 입력 받기
 
-        seconds = days * 24 * 60 * 60;// 값 계산
+        total = kor + eng + math; // 합계 계산
 
-        System.out.println("Total seconds is " + seconds); // 결과 출력 구문
+        average = total / 3.0; // 평균 계산
 
-        sc.close(); // 스캐너 사용 해제
+        System.out.println("Total point is " + total); // 결과 출력 구문
+        System.out.printf("The average  is %.1f\n", average); // 결과 출력 구문
+
+        sc.close(); // 스캐너 사용 종료
     }
 }
