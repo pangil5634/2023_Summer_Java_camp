79a51e6b8f1db528600ecf216bfc37ebea199637
pangil5634 <oksk6681@naver.com>
2023-07-11 21:40:23 +0900

    230711 B01 Complete

--- Main.java
+++ Main.java
@@ -2,32 +2,30 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Main stepA = new Main();
-        stepA.a07();
+        Main stepB = new Main();
+        stepB.b01();
     }
 
-    void a07() {
-        int gigabytes; // 용량(기가바이트 단위)
-        int megabytes; // 용량(메가바이트 단위)
-        int kilobytes; // 용량(킬로바이트 단위)
-        long bytes; // 용량(바이트 단위)
+    void b01() {
+        int birth_year; // 태어난 년도
+        int age; // 나이
 
         Scanner sc = new Scanner(System.in); // 스캐너 사용 선언
 
-        System.out.print("Enter file size as gigabytes > "); // 입력을 위한 출력 구문
+        System.out.print("Enter a birth year > "); // 입력을 위한 출력 구문
+        birth_year = sc.nextInt(); // birth_year 변수에 사용자로부터 값 입력 받기
 
-        gigabytes = sc.nextInt(); // gigabytes 변수에 사용자로부터 값 입력 받기
+        age = 2023 - birth_year + 1; // 나이 계산
 
-        megabytes = gigabytes * 1024; // 값 계산
-        kilobytes = megabytes * 1024; // 값 계산
-        bytes = (long) kilobytes * 1024; // 값 계산
-
-        System.out.println("Your file size is"); // 결과 출력 구문
-        System.out.println(megabytes + " MB"); // 결과 출력 구문
-        System.out.println(kilobytes + " KB"); // 결과 출력 구문
-        System.out.println(bytes + " B"); // 결과 출력 구문
+        ageResult(age); // ageResult 함수 호출
 
         sc.close(); // 스캐너 사용 해제
+    }
 
+    void ageResult(int age) { // 미성년자 판정 함수
+        if (age < 20) // age가 20 미만인지 판단
+            System.out.println("You are not adult"); // true
+        else if (age >= 20) // age가 20 이상인지 판단
+            System.out.println("You are adult"); // true
     }
 }
--- StepB/B01.java
+++ StepB/B01.java
@@ -1,17 +1,17 @@
-package StepA;
+package StepB;
 
 import java.util.Scanner;
 
-class A01 {
+public class B01 {
     public static void main(String[] args) {
-        A01 stepA = new A01();
-        stepA.a01();
+        B01 stepB = new B01();
+        stepB.b01();
     }
 
-    void a01() {
-
+    void b01() {
         int birth_year; // 태어난 년도
         int age; // 나이
+
         Scanner sc = new Scanner(System.in); // 스캐너 사용 선언
 
         System.out.print("Enter a birth year > "); // 입력을 위한 출력 구문
@@ -19,8 +19,16 @@ class A01 {
 
         age = 2023 - birth_year + 1; // 나이 계산
 
-        System.out.println("You are " + age + " years old."); // 결과 출력 구문
+        ageResult(age);
 
         sc.close(); // 스캐너 사용 해제
     }
+
+    void ageResult(int age) {
+        if (age < 20)
+            System.out.println("You are not adult");
+        else if (age >= 20)
+            System.out.println("You are adult");
+    }
+
 }
