633b53f70e3c41a091e99f6190a1da6d8b7df0d6
pangil5634 <oksk6681@naver.com>
2023-07-12 17:07:39 +0900

    230712 D06 Complete

--- Main.java
+++ Main.java
@@ -3,46 +3,44 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Main stepD = new Main();
-        stepD.d05();
+        stepD.d06();
     }
 
-    void d05() {
-        int width = 1, height = 1; // 가로크기, 세로크기
-        int count1 = 0; // "정사각형"의 개수
-        int count2 = 0; // "좌우로 길쭉한 직사각형"의 개수
-        int count3 = 0; // "위아래로 길쭉한 직사각형"의 개수
-        int count4 = 0; // "일반적인 가로형 직사각형"의 개수
-        int count5 = 0; // "일반적인 세로형 직사각형"의 개수
+    void d06() {
+
+        double m2_area = 0; // 면적 (제곱미터)
+        double pyung_area = 0; // 면적 (평수)
+        int count1 = 0; // 소형 아파트 개수
+        int count2 = 0; // 중소형 아파트 개수
+        int count3 = 0; // 중형 아파트 개수
+        int count4 = 0; // 대형 아파트 개수
+        int i = 0; // 반복문을 위한 변수
 
         Scanner sc = new Scanner(System.in); // 스캐너 사용 선언
 
-        while (true) {
-            System.out.print("Enter width, height of Rectangle > "); // 입력을 위한 출력 구문
-            width = sc.nextInt(); // width 변수에 사용자로부터 값 입력 받기
-            height = sc.nextInt(); // height 변수에 사용자로부터 값 입력 받기
-            if (width <= 0 || height <= 0) { // width 혹은 height 값이 이하일 경우
-                break;
-            } else {
-                if (width == height) { // "정사각형"의 개수
-                    count1++;
-                } else if (width >= height * 2) { // "좌우로 길쭉한 직사각형"의 개수
-                    count2++;
-                } else if (height >= width * 2) { // "위아래로 길쭉한 직사각형"의 개수
-                    count3++;
-                } else if (width > height) { // "일반적인 가로형 직사각형"의 개수
-                    count4++;
-                } else if (height > width) { // "일반적인 세로형 직사각형"의 개수
-                    count5++;
-                }
+        for (i = 1; i <= 10; i++) {
+            System.out.print("Enter area of Apartment(m^2) > "); // 입력을 위한 출력 구문
+            m2_area = sc.nextDouble(); // m2_area 변수에 사용자로부터 값 입력 받기
+            pyung_area = m2_area / 3.305; // 면적(평수) 계산
+
+            System.out.printf("Area of Apartment(pyung) %.1f\n", pyung_area); // 결과 출력 구문
+
+            if (pyung_area < 15) { // 소형 아파트인지
+                count1++;
+            } else if (pyung_area < 30) { // 중소형 아파트인지
+                count2++;
+            } else if (pyung_area < 50) { // 중형 아파트인지
+                count3++;
+            } else { // 대형 아파트인지
+                count4++;
             }
         }
 
         // 결과 출력 구문
-        System.out.println("Count of square is " + count1);
-        System.out.println("Count of an oblong rectangle from side to side is " + count2);
-        System.out.println("Count of an oblong rectangle from top to bottom is " + count3);
-        System.out.println("Count of a horizontal rectangle is " + count4);
-        System.out.println("Count of a vertical rectangle is " + count5);
+        System.out.println("Count of small-sized apartment is " + count1);
+        System.out.println("Count of small and medium-sized apartment is " + count2);
+        System.out.println("Count of medium-sized apartment is " + count3);
+        System.out.println("Count of big-sized apartment is " + count4);
 
         sc.close(); // 스캐너 사용 해제
 
--- StepD/D06.java
+++ StepD/D06.java
@@ -10,5 +10,41 @@ public class D06 {
 
     void d06() {
 
+        double m2_area = 0; // 면적 (제곱미터)
+        double pyung_area = 0; // 면적 (평수)
+        int count1 = 0; // 소형 아파트 개수
+        int count2 = 0; // 중소형 아파트 개수
+        int count3 = 0; // 중형 아파트 개수
+        int count4 = 0; // 대형 아파트 개수
+        int i = 0; // 반복문을 위한 변수
+
+        Scanner sc = new Scanner(System.in); // 스캐너 사용 선언
+
+        for (i = 1; i <= 10; i++) {
+            System.out.print("Enter area of Apartment(m^2) > "); // 입력을 위한 출력 구문
+            m2_area = sc.nextDouble(); // m2_area 변수에 사용자로부터 값 입력 받기
+            pyung_area = m2_area / 3.305; // 면적(평수) 계산
+
+            System.out.printf("Area of Apartment(pyung) %.1f\n", pyung_area); // 결과 출력 구문
+
+            if (pyung_area < 15) { // 소형 아파트인지
+                count1++;
+            } else if (pyung_area < 30) { // 중소형 아파트인지
+                count2++;
+            } else if (pyung_area < 50) { // 중형 아파트인지
+                count3++;
+            } else { // 대형 아파트인지
+                count4++;
+            }
+        }
+
+        // 결과 출력 구문
+        System.out.println("Count of small-sized apartment is " + count1);
+        System.out.println("Count of small and medium-sized apartment is " + count2);
+        System.out.println("Count of medium-sized apartment is " + count3);
+        System.out.println("Count of big-sized apartment is " + count4);
+
+        sc.close(); // 스캐너 사용 해제
+
     }
 }
