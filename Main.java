2205c637dccbf7bd89c2ee373995511383249d3f
pangil5634 <oksk6681@naver.com>
2023-07-15 14:27:23 +0900

    230715 E05 Complete

--- Main.java
+++ Main.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Main stepE = new Main();
-        stepE.e04();
+        stepE.e05();
     }
 
-    void e04() {
+    void e05() {
         int mode = 0; // 출력모드(1: 홀수단, 2: 짝수단)
+        int column; // 열 개수
         int i, j = 0; // 반복문 사용을 위한 변수
 
         Scanner sc = new Scanner(System.in);
@@ -16,12 +17,16 @@ class Main {
         System.out.print("Enter a type of gugudan (1: odd, 2: even) > ");
         mode = sc.nextInt();
 
+        // 열 개수 입력 받기
+        System.out.print("Enter a numbers of column > ");
+        column = sc.nextInt();
+
         // 결과 출력
         if (mode == 1) {
             for (i = 3; i <= 9; i += 2) {
                 for (j = 1; j <= 9; j++) {
 
-                    if (j % 3 == 0)
+                    if (j % column == 0)
                         System.out.printf("%d X %d = %d\n", i, j, i * j);
                     else {
                         System.out.printf("%d X %d = %d\t", i, j, i * j);
@@ -29,12 +34,13 @@ class Main {
                 }
                 if (i != 9)
                     System.out.printf("\n");
+
             }
         } else if (mode == 2) {
             for (i = 2; i <= 8; i += 2) {
                 for (j = 1; j <= 9; j++) {
 
-                    if (j % 3 == 0)
+                    if (j % column == 0)
                         System.out.printf("%d X %d = %d\n", i, j, i * j);
                     else {
                         System.out.printf("%d X %d = %d\t", i, j, i * j);
