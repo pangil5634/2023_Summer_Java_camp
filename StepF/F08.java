39b8ff3eb7eb1fc0ed2485c22ef49fed1f98e954
pangil5634 <oksk6681@naver.com>
2023-07-17 16:33:25 +0900

    230717 F08 Complete

---
+++ StepF/F08.java
@@ -0,0 +1,14 @@
+package StepF;
+
+import java.util.Scanner;
+
+class F08 {
+    public static void main(String[] args) {
+        F08 stepF = new F08();
+        stepF.f08();
+    }
+
+    void f08() {
+
+    }
+}
