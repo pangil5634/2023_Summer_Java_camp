4685e7147366a4b6f5da69d5382898fc2bc3a0d3
pangil5634 <oksk6681@naver.com>
2023-07-18 17:04:40 +0900

    230718 G01 Test

---
+++ Main/Main.java
@@ -0,0 +1,25 @@
+package Main;
+
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Main stepG = new Main();
+        stepG.G01();
+    }
+
+    void G01() {
+        Data1 data1 = new Data1();
+        Scanner sc = new Scanner(System.in);
+
+        data1.mainWhile();
+
+        System.out.println();
+        data1.printAge();
+        System.out.println();
+        data1.printResult();
+        sc.close();
+    }
+
+}
