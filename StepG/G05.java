f8dbf26095d69654be5c83c74b6b8dd41adf15ed
pangil5634 <oksk6681@naver.com>
2023-07-18 21:42:36 +0900

    230718 G05 COMPLETE

--- StepG/G05.java
+++ StepG/G05.java
@@ -9,6 +9,23 @@ class G05 {
 
     void g05() {
         G05Sub subclass = new G05Sub(); // 인스턴스 생성
+        int total_charge = 0; // 이용료 총액
+
+        while (true) {
+            // 시간, 분 입력 받기
+            subclass.getTime();
+
+            // 기준에 맞게 출력하기
+            if (subclass.getHour() == 0 && subclass.getMinute() == 0) {
+                System.out.println("Your total charge is " + total_charge);
+                break;
+            } else {
+                int charge = subclass.calculateCharge();
+                System.out.println("Your charge is " + charge);
+                total_charge += charge;
+            }
+            System.out.println();
+        }
 
     }
 
--- StepG/G05Sub.java
+++ StepG/G05Sub.java
@@ -3,5 +3,65 @@ package StepG;
 import java.util.Scanner;
 
 public class G05Sub {
+    Scanner sc = new Scanner(System.in);
+    private int hour = 1; // 시간
+    private int minute = 1;// 분
+
+    // Encapsulation
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    // 사용자로부터 시간과 분을 입력 받는 메소드
+    public void getTime() {
+
+        System.out.print("Enter hours and minutes of use > ");
+        hour = sc.nextInt();
+        minute = sc.nextInt();
+    }
+
+    // 이용료 계산하는 메소드
+    public int calculateCharge() {
+        int charge = caclculateMoney(); // 이용료
+        int discount = 0; // 할인 금액
+        if ((hour == 2 && minute >= 0) && (hour == 2 && minute <= 59)) {
+            discount = charge / 100 * 5;
+        } else if ((hour >= 3 && minute >= 0) && (hour <= 4 && minute <= 59)) {
+            discount = charge / 10;
+        } else if (hour >= 5 && minute >= 0) {
+            discount = charge / 10 * 2;
+        }
+        charge -= discount;
+
+        return charge;
+    }
+
+    // 할인 전 이용료 금액 계산하는 메소드
+    public int caclculateMoney() {
+        int charge = 0;
+
+        charge = hour * 2000;
+        if (minute != 0) {
+            if (minute / 30 == 0 || minute % 30 == 0) {
+                charge += 1000;
+            } else if (minute / 30 == 1) {
+                charge += 2000;
+            }
+        }
+
+        return charge;
+    }
 
 }
