23ec5ec5d417c8bd859e69d3e9f9c80938628a6d
pangil5634 <oksk6681@naver.com>
2023-07-27 14:20:09 +0900

    230727 H03 Complete

--- StepH/H03.java
+++ StepH/H03.java
@@ -6,24 +6,29 @@ public class H03 {
 
     public static void main(String[] args) {
         H03 stepH = new H03();
-        stepH.h02();
+        stepH.h03();
     }
 
     Scanner sc = new Scanner(System.in);
 
-    void h02() {
+    void h03() {
 
-        String onemore = "Y"; // 반복여부를 입력하는 문자 (Y/N)
+        int lotto_com[] = new int[6]; // 컴퓨터가 만들어 낸 로또 번호 리스트
+        int lotto_user[] = new int[6]; // 사용자가 입력한 로또 번호 리스트
 
         H03Sub h02sub = new H03Sub();
-        while (true) {
-            if (onemore.equals("Y") || onemore.equals("y"))
-                h02sub.setLotto();
-            else
-                break;
-            System.out.print("Do you want to generate more lotto numbers? (Y/N) > ");
-            onemore = sc.next();
-        }
+
+        // 컴퓨터 로또 번호 구하기
+        lotto_com = h02sub.getCom(lotto_com);
+
+        // 사용자 로또 번호 구하기
+        lotto_user = h02sub.getUser(lotto_user);
+
+        // 결과 출력하기 (컴퓨터 로또 번호)
+        h02sub.printResult(lotto_com, lotto_user);
+
+        // 결과 출력하기 (매칭된 개수)
+        h02sub.compareMatch(lotto_com, lotto_user);
 
     }
 
--- StepH/H03Sub.java
+++ StepH/H03Sub.java
@@ -6,19 +6,19 @@ public class H03Sub {
 
     Scanner sc = new Scanner(System.in);
 
-    // 로또 점수를 만드는 메소드
-    void setLotto() {
-        int lotto[] = new int[6]; // 컴퓨터가 만들어 낸 로또 번호 리스트
+    // 컴퓨터 로또 번호 구하기
+    int[] getCom(int[] lotto_com) {
+
         int count = 0; // 현재 만들어지고 있는 로또 번호의 순서(0, 1, 2, 3, 4, 5)
 
         while (count < 6) {
             int temp = (int) ((Math.random() * 45) + 1);
-            lotto[count] = temp;
+            lotto_com[count] = temp;
             count++;
 
             // 중복 체크 과정
             for (int i = 0; i < count - 1; i++) {
-                if (temp == lotto[i]) {
+                if (temp == lotto_com[i]) {
                     count--;
                     break;
                 }
@@ -26,13 +26,57 @@ public class H03Sub {
 
         }
 
+        return lotto_com;
+    }
+
+    // 사용자 로또 번호 구하기
+    int[] getUser(int[] lotto_user) {
+
+        int count = 0; // 현재 만들어지고 있는 로또 번호의 순서(0, 1, 2, 3, 4, 5)
+
+        while (count < 6) {
+            System.out.printf("Enter your %dth lotto number > ", count + 1);
+            int temp = sc.nextInt();
+            lotto_user[count] = temp;
+            count++;
+
+            // 중복 체크 과정
+            for (int i = 0; i < count - 1; i++) {
+                if (temp == lotto_user[i]) {
+                    System.out.println("Invalid input!");
+                    count--;
+                    break;
+                }
+            }
+
+        }
+        return lotto_user;
+    }
+
+    // 결과 출력하기 (컴퓨터 로또 번호)
+    void printResult(int[] lotto_com, int[] lotto_user) {
+        System.out.print("Lotto numbers of this week are ");
         // 결과 출력
         for (int i = 0; i < 6; i++) {
-            if (i == 5) {
-                System.out.print(lotto[i] + "\n");
-            } else
-                System.out.print(lotto[i] + ", ");
-        }
 
+            if (i == 5) {
+                System.out.print(lotto_com[i] + "\n");
+            } else
+                System.out.print(lotto_com[i] + " ");
+        }
+    }
+
+    // 결과 출력하기 (매칭된 개수)
+    void compareMatch(int[] lotto_com, int[] lotto_user) {
+        int match_count = 0; // 일치하는 로또 번호의 갯수 (0~6)
+        for (int i = 0; i < lotto_com.length; i++) {
+            for (int j = 0; j < lotto_user.length; j++) {
+                if (lotto_com[i] == lotto_user[j]) {
+                    match_count++;
+                    break;
+                }
+            }
+        }
+        System.out.printf("There are %d matching lotto numbers\n", match_count);
     }
 }
