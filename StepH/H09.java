78d99532948f054e1e4cf5ff641aaac003b854ed
pangil5634 <oksk6681@naver.com>
2023-07-30 10:59:03 +0900

    230730 H09 Complete

---
+++ StepH/H09.java
@@ -0,0 +1,27 @@
+package StepH;
+
+import java.util.Scanner;
+
+public class H09 {
+
+    public static void main(String[] args) {
+        H09 stepH = new H09();
+        stepH.h09();
+    }
+
+    Scanner sc = new Scanner(System.in);
+
+    /**
+     * 
+     */
+    void h09() {
+        H09Sub h09sub = new H09Sub();
+
+        String input; // 입력받는 문자열(사람들 간은 ### 로 구분, 이름과 성별은 |로 구분)
+
+        String userList[]; // 사용자 리스트
+
+        String userInfo[]; // 한 사람의 정보
+    }
+
+}
---
+++ StepH/H09Sub.java
@@ -0,0 +1,9 @@
+package StepH;
+
+import java.util.Scanner;
+
+public class H09Sub {
+
+    Scanner sc = new Scanner(System.in);
+
+}
